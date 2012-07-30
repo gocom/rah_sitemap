0f964b22010e85493485f4bdb7a97a306faccb12
Jukka Svahn <web@rahinaa.biz>
2012-07-31 00:58:14 +0300

    Introduces links module.
    The module adds links to the sitemap.

---
+++ extending/links/links.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Links module for rah_sitemap.
+ * 
+ * @package rah_sitemap
+ * @author Jukka Svahn <http://rahforum.biz/>
+ * @copyright (c) 2012 Jukka Svahn
+ * @license GLPv2
+ */
+
+	if(defined('txpinterface')) {
+		new rah_sitemap__links();
+	}
+
+class rah_sitemap__links {
+
+	/**
+	 * Constructor
+	 */
+
+	public function __construct() {
+		register_callback(array($this, 'urlset'), 'rah_sitemap.urlset');
+	}
+
+	/**
+	 * Adds links to the sitemap
+	 */
+
+	public function urlset() {
+		$local = str_replace(array('%', '_'), array('\\%', '\\_'), doSlash(hu));
+
+		$rs = 
+			safe_rows(
+				'url, date',
+				'txp_link',
+				"category='rah_sitemap' or url LIKE '".$local."_%' or url LIKE '/_%'"
+			);
+
+		foreach($rs as $a) {
+			rah_sitemap::get()->url($a['url'], $a['date']);
+		}
+	}
+}
+
+?>
