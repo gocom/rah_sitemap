ad2d509851b30165c1213358f6f72ff1a3c3430d
Jukka Svahn <web@rahinaa.biz>
2014-03-21 11:35:31 +0200

    Coding style.

--- rah_sitemap.php
+++ rah_sitemap.php
@@ -59,18 +59,15 @@ class Rah_Sitemap
             'compress'                => array('yesnoradio', 0),
         );
 
-        if (!in_array('rah_sitemap_include_in', getThings('describe '.safe_pfx('txp_section'))))
-        {
+        if (!in_array('rah_sitemap_include_in', getThings('describe '.safe_pfx('txp_section')))) {
             safe_alter('txp_section', 'ADD rah_sitemap_include_in TINYINT(1) NOT NULL DEFAULT 1');
         }
 
-        if (!in_array('rah_sitemap_include_in', getThings('describe '.safe_pfx('txp_category'))))
-        {
+        if (!in_array('rah_sitemap_include_in', getThings('describe '.safe_pfx('txp_category')))) {
             safe_alter('txp_category', 'ADD rah_sitemap_include_in TINYINT(1) NOT NULL DEFAULT 1');
         }
 
-        if (in_array(PFX.'rah_sitemap_prefs', getThings('SHOW TABLES')))
-        {
+        if (in_array(PFX.'rah_sitemap_prefs', getThings('SHOW TABLES'))) {
             $update = array(
                 'sections'   => array(),
                 'categories' => array(),
@@ -78,68 +75,54 @@ class Rah_Sitemap
 
             $rs = safe_rows('name, value', 'rah_sitemap_prefs', '1=1');
 
-            foreach ($rs as $a)
-            {
-                if (trim($a['value']) === '')
-                {
+            foreach ($rs as $a) {
+                if (trim($a['value']) === '') {
                     continue;
                 }
 
-                if ($a['name'] == 'articlecategories')
-                {
-                    foreach (do_list($a['value']) as $v)
-                    {
+                if ($a['name'] == 'articlecategories') {
+                    foreach (do_list($a['value']) as $v) {
                         $opt['exclude_fields'][1][] = 'Category1: ' . $v;
                         $opt['exclude_fields'][1][] = 'Category2: ' . $v;
                     }
-                }
-
-                else if ($a['name'] == 'articlesections')
-                {
-                    foreach (do_list($a['value']) as $v)
-                    {
+                } elseif ($a['name'] == 'articlesections') {
+                    foreach (do_list($a['value']) as $v) {
                         $opt['exclude_fields'][1][] = 'Section: ' . $v;
                     }
-                }
-
-                else if ($a['name'] == 'sections')
-                {
+                } elseif ($a['name'] == 'sections') {
                     $update['sections'] = do_list($a['value']);
-                }
-
-                else if ($a['name'] == 'categories')
-                {
-                    foreach (do_list($a['value']) as $v)
-                    {
+                } elseif ($a['name'] == 'categories') {
+                    foreach (do_list($a['value']) as $v) {
                         $v = explode('_||_', $v);
                         $update['categories'][$v[0]][] = end($v);
                     }
-                }
-
-                else if (isset($opt[$a['name']]))
-                {
+                } elseif (isset($opt[$a['name']])) {
                     $opt[$a['name']][1] = $a['value'];
                 }
             }
 
-            @$rs = safe_column('url', 'rah_sitemap', '1=1');
+            @$rs = safe_column('url', 'rah_sitemap', '1 = 1');
 
-            if ($rs)
-            {
+            if ($rs) {
                 $opt['urls'][1] = implode(', ', $rs);
             }
 
-            if ($update['categories'])
-            {
-                foreach ($update['categories'] as $type => $categories)
-                {
-                    safe_update('txp_category', 'rah_sitemap_include_in=0', "type='".doSlash($type)."' and name IN(".implode(',', quote_list($categories)).")");
+            if ($update['categories']) {
+                foreach ($update['categories'] as $type => $categories) {
+                    safe_update(
+                        'txp_category',
+                        'rah_sitemap_include_in = 0',
+                        "type = '".doSlash($type)."' and name IN(".implode(',', quote_list($categories)).")"
+                    );
                 }
             }
 
-            if ($update['sections'])
-            {
-                safe_update('txp_section', 'rah_sitemap_include_in=0', 'name IN('.implode(',', quote_list($update['sections'])).')');
+            if ($update['sections']) {
+                safe_update(
+                    'txp_section',
+                    'rah_sitemap_include_in = 0',
+                    'name IN('.implode(',', quote_list($update['sections'])).')'
+                );
             }
 
             @safe_query('DROP TABLE IF EXISTS '.safe_pfx('rah_sitemap'));
@@ -148,14 +131,11 @@ class Rah_Sitemap
 
         $position = 260;
 
-        foreach ($opt as $name => $val)
-        {
+        foreach ($opt as $name => $val) {
             $n = 'rah_sitemap_'.$name;
 
-            if (get_pref($n, false) === false)
-            {
-                if (is_array($val[1]))
-                {
+            if (get_pref($n, false) === false) {
+                if (is_array($val[1])) {
                     $val[1] = implode(',', $val[1]);
                 }
 
@@ -188,7 +168,7 @@ class Rah_Sitemap
         register_callback(array($this, 'install'), 'plugin_lifecycle.rah_sitemap', 'installed');
         register_callback(array($this, 'uninstall'), 'plugin_lifecycle.rah_sitemap', 'deleted');
         register_callback(array($this, 'prefs'), 'plugin_prefs.rah_sitemap');
-        register_callback(array($this, 'page_handler'), 'textpattern');
+        register_callback(array($this, 'pageHandler'), 'textpattern');
         register_callback(array($this, 'renderSectionOptions'), 'section_ui', 'extend_detail_form');
         register_callback(array($this, 'renderCategoryOptions'), 'category_ui', 'extend_detail_form');
         register_callback(array($this, 'saveSection'), 'section', 'section_save');
@@ -199,26 +179,25 @@ class Rah_Sitemap
     }
 
     /**
-     * Handles returning the sitemap.
+     * Handles routing requests to the sitemap.
      */
 
-    public function page_handler()
+    public function pageHandler()
     {
         global $pretext;
 
-        if (!gps('rah_sitemap') && basename($pretext['request_uri'], '.gz') !== 'sitemap.xml')
-        {
+        if (!gps('rah_sitemap') && basename($pretext['request_uri'], '.gz') !== 'sitemap.xml') {
             return;
         }
 
-        return $this->populateArticleFields()->get_sitemap();
+        return $this->populateArticleFields()->sendSitemap();
     }
 
     /**
      * Generates and outputs the sitemap.
      */
 
-    protected function get_sitemap()
+    protected function sendSitemap()
     {
         $this->addUrl(hu);
 
@@ -228,10 +207,8 @@ class Rah_Sitemap
             "name != 'default' and rah_sitemap_include_in = 1 order by name asc"
         );
 
-        if ($rs)
-        {
-            while ($a = nextRow($rs))
-            {
+        if ($rs) {
+            while ($a = nextRow($rs)) {
                 $this->addUrl(pagelinkurl(array('s' => $a['name'])));
             }
         }
@@ -242,47 +219,38 @@ class Rah_Sitemap
             "name != 'root' and rah_sitemap_include_in = 1 order by name asc"
         );
 
-        if ($rs)
-        {
-            while ($a = nextRow($rs))
-            {
+        if ($rs) {
+            while ($a = nextRow($rs)) {
                 $this->addUrl(pagelinkurl(array('c' => $a['name'], 'context' => $a['type'])));
             }
         }
 
         $sql = array('Status >= 4');
 
-        foreach (do_list(get_pref('rah_sitemap_exclude_fields')) as $field)
-        {
-            if ($field)
-            {
+        foreach (do_list(get_pref('rah_sitemap_exclude_fields')) as $field) {
+            if ($field) {
                 $f = explode(':', $field);
                 $n = strtolower(trim($f[0]));
 
-                if (isset($this->articleFields[$n]))
-                {
+                if (isset($this->articleFields[$n])) {
                     $sql[] = $this->articleFields[$n]." NOT LIKE '".doSlash(trim(implode(':', array_slice($f, 1))))."'";
                 }
             }
         }
 
-        if (get_pref('rah_sitemap_exclude_sticky_articles'))
-        {
+        if (get_pref('rah_sitemap_exclude_sticky_articles')) {
             $sql[] = 'Status != 5';
         }
 
-        if (!get_pref('rah_sitemap_future_articles'))
-        {
+        if (!get_pref('rah_sitemap_future_articles')) {
             $sql[] = 'Posted <= now()';
         }
 
-        if (!get_pref('rah_sitemap_past_articles'))
-        {
+        if (!get_pref('rah_sitemap_past_articles')) {
             $sql[] = 'Posted >= now()';
         }
 
-        if (!get_pref('rah_sitemap_expired_articles'))
-        {
+        if (!get_pref('rah_sitemap_expired_articles')) {
             $sql[] = "(Expires = ".NULLDATETIME." or Expires >= now())";
         }
 
@@ -292,18 +260,14 @@ class Rah_Sitemap
             implode(' and ', $sql) . ' order by Posted desc'
         );
 
-        if ($rs)
-        {
-            while ($a = nextRow($rs))
-            {
+        if ($rs) {
+            while ($a = nextRow($rs)) {
                 $this->addUrl(permlinkurl($a), (int) max($a['uLastMod'], $a['uPosted']));
             }
         }
 
-        foreach (do_list(get_pref('rah_sitemap_urls')) as $url)
-        {
-            if ($url)
-            {
+        foreach (do_list(get_pref('rah_sitemap_urls')) as $url) {
+            if ($url) {
                 $this->addUrl($url);
             }
         }
@@ -311,10 +275,8 @@ class Rah_Sitemap
         $urlset = array();
         callback_event_ref('rah_sitemap.urlset', '', 0, $urlset);
 
-        if ($urlset && is_array($urlset))
-        {
-            foreach ($urlset as $url => $lastmod)
-            {
+        if ($urlset && is_array($urlset)) {
+            foreach ($urlset as $url => $lastmod) {
                 $this->addUrl($url, $lastmod);
             }
         }
@@ -330,14 +292,13 @@ class Rah_Sitemap
         header('Content-type: text/xml; charset=utf-8');
 
         if (
-            get_pref('rah_sitemap_compress') && 
-            strpos(serverSet('HTTP_ACCEPT_ENCODING'), 'gzip') !== false && 
-            @extension_loaded('zlib') && 
-            @ini_get('zlib.output_compression') == 0 && 
+            get_pref('rah_sitemap_compress') &&
+            strpos(serverSet('HTTP_ACCEPT_ENCODING'), 'gzip') !== false &&
+            @extension_loaded('zlib') &&
+            @ini_get('zlib.output_compression') == 0 &&
             @ini_get('output_handler') != 'ob_gzhandler' &&
             !@headers_sent()
-        )
-        {
+        ) {
             header('Content-Encoding: gzip');
             $xml = gzencode($xml);
         }
@@ -354,32 +315,25 @@ class Rah_Sitemap
      * @return rah_sitemap
      */
 
-    protected function addUrl($url, $lastmod = null)
-    {
-        if (strpos($url, 'http://') !== 0 && strpos($url, 'https://') !== 0)
-        {
+    protected function addUrl($url, $lastmod = null) {
+        if (strpos($url, 'http://') !== 0 && strpos($url, 'https://') !== 0) {
             $url = hu.ltrim($url, '/');
         }
 
-        if (preg_match('/[\'"<>]/', $url))
-        {
+        if (preg_match('/[\'"<>]/', $url)) {
             $url = htmlspecialchars($url, ENT_QUOTES);
         }
 
-        if (isset($this->addUrlset[$url]))
-        {
+        if (isset($this->addUrlset[$url])) {
             return $this;
         }
 
-        if ($lastmod !== null)
-        {
-            if (!is_int($lastmod))
-            {
+        if ($lastmod !== null) {
+            if (!is_int($lastmod)) {
                 $lastmod = strtotime($lastmod);
             }
 
-            if ($lastmod !== false)
-            {
+            if ($lastmod !== false) {
                 $lastmod = safe_strftime('iso8601', $lastmod);
             }
         }
@@ -403,13 +357,11 @@ class Rah_Sitemap
     {
         $columns = (array) @getThings('describe '.safe_pfx('textpattern'));
 
-        foreach ($columns as $name)
-        {
+        foreach ($columns as $name) {
             $this->articleFields[strtolower($name)] = $name;
         }
 
-        foreach (getCustomFields() as $id => $name)
-        {
+        foreach (getCustomFields() as $id => $name) {
             $this->articleFields[$name] = 'custom_'.intval($id);
         }
 
@@ -424,10 +376,11 @@ class Rah_Sitemap
     {
         pagetop(gTxt('rah_sitemap'));
 
-        echo 
-            '<p>'.n.
-            '    <a href="?event=prefs&amp;step=advanced_prefs#prefs-rah_sitemap_exclude_fields">'.gTxt('rah_sitemap_view_prefs').'</a><br />'.n.
-            '    <a href="'.hu.'?rah_sitemap=sitemap">'.gTxt('rah_sitemap_view_sitemap').'</a>'.
+        echo '<p>'.n.
+            '<a href="?event=prefs&amp;step=advanced_prefs#prefs-rah_sitemap_exclude_fields">'.
+                gTxt('rah_sitemap_view_prefs').
+            '</a><br />'.n.
+            '<a href="'.hu.'?rah_sitemap=sitemap">'.gTxt('rah_sitemap_view_sitemap').'</a>'.
             '</p>';
     }
 
@@ -443,8 +396,7 @@ class Rah_Sitemap
 
     public function renderSectionOptions($event, $step, $void, $r)
     {
-        if ($r['name'] !== 'default')
-        {
+        if ($r['name'] !== 'default') {
             return inputLabel('rah_sitemap_include_in', yesnoradio('rah_sitemap_include_in', !empty($r['rah_sitemap_include_in']), '', ''), '', 'rah_sitemap_include_in');
         }
     }
