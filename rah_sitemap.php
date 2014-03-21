17b35c9dfed2247c395dfb71ba2b8ca0d349f86a
Jukka Svahn <web@rahinaa.biz>
2014-03-21 11:07:15 +0200

    Updated header comments.

--- rah_sitemap.php
+++ rah_sitemap.php
@@ -1,15 +1,28 @@
 <?php
 
+/*
+ * rah_sitemap - Flat templates for Textpattern CMS
+ * https://github.com/gocom/rah_sitemap
+ *
+ * Copyright (C) 2014 Jukka Svahn
+ *
+ * This file is part of rah_sitemap.
+ *
+ * rah_sitemap is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, version 2.
+ *
+ * rah_sitemap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with rah_sitemap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 /**
- * Rah_sitemap plugin for Textpattern CMS.
- *
- * @author  Jukka Svahn
- * @license GNU GPLv2
- * @link    http://rahforum.biz/plugins/rah_sitemap
- *
- * Copyright (C) 2013 Jukka Svahn http://rahforum.biz
- * Licensed under GNU General Public License version 2
- * http://www.gnu.org/licenses/gpl-2.0.html
+ * Main plugin class.
  */
 
 class rah_sitemap
