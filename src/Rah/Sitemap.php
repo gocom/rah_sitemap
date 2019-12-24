ade4847415e00c8040f94c46a3051502c606174b
Sebastian Spautz <sebastian@human-injection.de>
2019-12-24 12:57:17 +0100

    Fix a bug resolving the value for sitemap_include_in in sections

--- src/Rah/Sitemap.php
+++ src/Rah/Sitemap.php
@@ -346,9 +346,10 @@ final class Rah_Sitemap
     public function renderSectionOptions($event, $step, $void, $r): string
     {
         if ($r['name'] !== 'default') {
+            $value = empty($r['rah_sitemap_include_in'])? 0 : $r['rah_sitemap_include_in'];
             return inputLabel(
                 'rah_sitemap_include_in',
-                yesnoradio('rah_sitemap_include_in', !empty($r['rah_sitemap_include_in']), '', ''),
+                yesnoradio('rah_sitemap_include_in', $value, '', ''),
                 '',
                 'rah_sitemap_include_in'
             );
